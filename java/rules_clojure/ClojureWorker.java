e659ca6ea0a7a871ad7d26b2e296e1467b87d8fc
Allen Rohner <allen@griffin.sh>
2022-07-19 13:39:03 +0100

    WIP

--- java/rules_clojure/ClojureWorker.java
+++ java/rules_clojure/ClojureWorker.java
@@ -136,7 +136,7 @@ class ClojureWorker  {
 
     public static void main(String [] args) throws Exception
     {
-        real_stderr.printf("args: %s", Arrays.toString(args));
+        System.err.printf("args: %s", Arrays.toString(args));
 	if (args.length > 0 && args[0].equals("--persistent_worker")) {
 	    persistentWorkerMain(args);
 	} else {
