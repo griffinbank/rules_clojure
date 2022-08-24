09f84a6b0795351b97b527e5378e05156292ac76
Allen Rohner <allen@griffin.sh>
2022-08-24 20:20:02 +0100

    GAV strategy

--- src/rules_clojure/persistent_classloader.clj
+++ src/rules_clojure/persistent_classloader.clj
@@ -1,5 +1,6 @@
 (ns rules-clojure.persistent-classloader
-  (:require [clojure.java.io :as io]
+  (:require [clojure.data]
+            [clojure.java.io :as io]
             [clojure.set :as set]
             [clojure.tools.namespace.dependency :as dep]
             [rules-clojure.util :as util]
@@ -64,10 +65,18 @@
       (return-classloader [this aot-nses classpath classloader]
         nil))))
 
+(defn clear-dead-refs [cache]
+  (swap! cache (fn [cache]
+                 (->> cache
+                      (filter (fn [[k v]]
+                                (.get v)))
+                      (into {})))))
+
 (defn cache-classloader [cache key cl]
   {:pre [(map? @cache)]
    :post [(map? @cache)]}
-  (swap! cache assoc key (SoftReference. cl)))
+  (swap! cache assoc key (SoftReference. cl))
+  (clear-dead-refs cache))
 
 (defn claim-classloader
   "Attempt to claim a cached classloader with key. Returns the classloader if successful, else nil"
@@ -83,64 +92,70 @@
                        cache))))
     @claimed))
 
+(defn parse-GAV-1
+  "Given the path to a path, attempt to parse out maven Group Artifact Version coordinates, returns nil if it doesn't appear to be in a maven dir"
+  [p]
+  (let [[_match group artifact version] (re-find #"repository/(.+)/([^\/]+)/([^\/]+)/\2-\3.jar" p)]
+    (when _match
+      [(str group "/" artifact) version])))
+
+(defn GAV-map
+  "given a classpath, return a map of all parsed GAV coordinates"
+  [classpath]
+  (->> classpath
+       (map parse-GAV-1)
+       (filter identity)
+       (into {})))
+
+(defn compatible-classpaths? [c1 c2]
+  (let [gav-1 (GAV-map c1)
+        gav-2 (GAV-map c2)
+        common-keys (set/intersection (set (keys gav-1)) (set (keys gav-2)))]
+    (= (select-keys gav-1 common-keys)
+       (select-keys gav-2 common-keys))))
+
 (defn new-classloader-cache [cache classpath]
   {:pre [(every? string? classpath)]}
   (let [cp-desired (set classpath)
-        ;; dirs will never be cached, so exclude
-        cp-jars (set (filter jar? cp-desired))
-        cp-dirs (set (remove jar? cp-desired))
-        caches (->> @cache
-                    (filter (fn [[k _ref]]
-                              (= #{} (set/difference k cp-jars)))))
+        cache-deref @cache
+        caches (->> cache-deref
+                    (filter (fn [[cp-cache _cl-ref]]
+                              (compatible-classpaths? cp-cache cp-desired))))
         cp-parent (when (seq caches)
                     (let [[cp-parent parent-ref] (apply max-key (fn [[cp-cache cl-ref]]
-                                                                  (count cp-cache)) caches)]
+                                                                  (count (set/intersection cp-desired cp-cache))) caches)]
                       cp-parent))
-        new-cache-cl (if-let [cl (and cp-parent (some-> (claim-classloader cache cp-parent) (.get)))]
-                       (do
-                         (doseq [p cp-jars]
-                           (add-url cl p))
-                         cl)
-                       (new-classloader- cp-jars))
-        cl-final (new-classloader- cp-dirs new-cache-cl)]
-    (util/shim-deref cl-final "clojure.core" "*clojure-version*")
-    (util/bind-compiler-loader cl-final)
-    cl-final))
-
-(defn new-classloader-cache-2 [cache classpath]
-  {:pre [(every? string? classpath)]}
-  (let [cp-desired (set classpath)
-        caches (->> @cache
-                    (filter (fn [[k _ref]]
-                              (= #{} (set/difference k cp-desired)))))
-        cp-parent (when (seq caches)
-                    (let [[cp-parent parent-ref] (apply max-key (fn [[cp-cache cl-ref]]
-                                                                  (count cp-cache)) caches)]
-                      cp-parent))
-        cl (if-let [cl (and cp-parent (some-> (claim-classloader cache cp-parent) (.get)))]
-                       (do
-                         (doseq [p cp-desired]
-                           (add-url cl p))
-                         cl)
-                       (new-classloader- cp-desired))]
+        cl (if (seq cache-deref)
+             (if cp-parent
+               (if-let [cl-ref (claim-classloader cache cp-parent)]
+                 (if-let [cl (.get cl-ref)]
+                   (let [cp-new (set/difference cp-desired cp-parent)]
+                     (doseq [p cp-new]
+                       (add-url cl p))
+                     (do
+                       ;; (println "hit")
+                       cl))
+                   (do
+                     ;; (println "cache-miss GC")
+                     (new-classloader- cp-desired)))
+                 (do
+                   ;; (println "cache failed claim. size:" (count cache-deref) )
+                   (new-classloader- cp-desired)))
+               (do
+                 ;; (println "cache miss incompatible:" (map (fn [[cp-cache _cl-ref]] (compatible-classpaths? cp-desired cp-cache)) cache-deref))
+                 (new-classloader- cp-desired)))
+             (do
+               ;; (println "cache-miss empty")
+               (new-classloader- cp-desired)))]
     (util/shim-deref cl "clojure.core" "*clojure-version*")
     (util/bind-compiler-loader cl)
     cl))
-
-(defn caching-naive []
-  ;; set of cp jars to a SoftReference to classloader
-  (let [cache (atom {})]
-    (reify ClassLoaderStrategy
-      (get-classloader[this classpath]
-        (new-classloader-cache cache classpath))
-      (return-classloader [this aot-nses classpath classloader]
-        (cache-classloader cache (set (filter jar? classpath)) (.getParent classloader))))))
 
 (defn caching-clean []
   (let [cache (atom {})]
     (reify ClassLoaderStrategy
       (get-classloader [this classpath]
-        (let [classloader (new-classloader-cache-2 cache classpath)]
+        (let [classloader (new-classloader-cache cache classpath)]
           classloader))
       (return-classloader [this aot-nses classpath classloader]
         (let [script (str `(do
@@ -148,8 +163,11 @@
                              (some (fn [n#]
                                      (rules-clojure.compile/contains-protocols? (symbol n#))) [~@aot-nses])))
               ret (util/shim-eval classloader script)]
-          (when-not ret
-            (cache-classloader cache (set (filter jar? classpath)) classloader)))))))
+          (if-not ret
+            (cache-classloader cache (set (filter jar? classpath)) classloader)
+            (do
+              ;; (println "AOT" aot-nses "discarding")
+              )))))))
 
 (defn new-classloader [classpath]
   (get-classloader (slow-naive) classpath))
