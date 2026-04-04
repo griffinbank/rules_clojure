ad64cab15e7defcf2144e75f15cc8ed0dbd39cec
Jamie English <jamienglish@gmail.com>
2026-04-04 16:20:18 +0100

    refactor(gen_srcs_bench): stream namespace generation and simplify src reset
    - Replace namespace accumulation with level-wise lazy generation/write loop\n- Replace manual file-seq reverse deletion with NIO walkFileTree directory delete\n- Keep namespace count via formula-based sum (no full namespace list in memory)

--- examples/gen_srcs_bench/generator.clj
+++ examples/gen_srcs_bench/generator.clj
@@ -1,7 +1,10 @@
 (ns generator
   (:require
    [clojure.java.io :as io]
-   [clojure.string :as str]))
+   [clojure.string :as str])
+  (:import
+   [java.nio.file Files Path FileVisitResult SimpleFileVisitor]
+   [java.nio.file.attribute BasicFileAttributes]))
 
 (set! *warn-on-reflection* true)
 
@@ -17,21 +20,24 @@
       (map str chars)
       (for [parent parents c chars] (str parent "." c)))))
 
-(defn generate-namespaces [width depth]
-  (reduce (fn [names d]
-            (concat names (generate-names-at-depth width d (take-last (int (Math/pow width (dec d))) names))))
-          []
-          (range 1 (inc depth))))
+(defn namespace-levels [width depth]
+  (let [chars (vec (map str (take width (alphabet-chars))))]
+    (->> (iterate (fn [parents]
+                    (for [parent parents
+                          c chars]
+                      (str parent "." c)))
+                  chars)
+         (take depth))))
 
-(defn has-children? [ns-name max-depth]
-  (< (count (str/split ns-name #"\\.")) max-depth))
+(defn count-namespaces [width depth]
+  (reduce + (take depth (iterate #(* % width) width))))
 
 (defn- children-of [ns-name width]
   (let [chars (take width (alphabet-chars))]
     (map #(str ns-name "." %) chars)))
 
-(defn- generate-ns-form-str [ns-name max-depth width]
-  (if (has-children? ns-name max-depth)
+(defn- generate-ns-form-str [ns-name depth-level max-depth width]
+  (if (< depth-level max-depth)
     (let [children (children-of ns-name width)
           requires (str/join "\n   "
                              (for [child children]
@@ -44,8 +50,8 @@
             (for [i (range 1 (inc num-functions))]
               (str "(defn fn-" i "\n  \"Generated function " i " for " ns-name "\"\n  [x]\n  (assoc x :" ns-name " " i "))"))))
 
-(defn- generate-source [ns-name max-depth width]
-  (str (generate-ns-form-str ns-name max-depth width)
+(defn- generate-source [ns-name depth-level max-depth width]
+  (str (generate-ns-form-str ns-name depth-level max-depth width)
        "\n\n"
        (generate-functions-str ns-name 100)
        "\n"))
@@ -53,28 +59,32 @@
 (defn- ns->file-path [ns-name]
   (str (str/replace ns-name "." "/") ".clj"))
 
-(defn- delete-generated-build-files! [^java.io.File src-dir]
-  (when (.exists src-dir)
-    (doseq [^java.io.File f (file-seq src-dir)
-            :when (and (.isFile f)
-                       (= "BUILD.bazel" (.getName f)))]
-      (io/delete-file f))))
+(defn- delete-tree! [^Path root]
+  (when (Files/exists root (make-array java.nio.file.LinkOption 0))
+    (Files/walkFileTree
+     root
+     (proxy [SimpleFileVisitor] []
+       (visitFile [^Path file ^BasicFileAttributes _attrs]
+         (Files/delete file)
+         FileVisitResult/CONTINUE)
+       (postVisitDirectory [^Path dir _exc]
+         (Files/delete dir)
+         FileVisitResult/CONTINUE)))))
 
 (defn reset-src! [^java.io.File src-dir]
-  (when (.exists src-dir)
-    (doseq [f (reverse (file-seq src-dir))]
-      (io/delete-file f)))
+  (delete-tree! (.toPath src-dir))
   (.mkdirs src-dir))
 
-(defn write-project! [namespaces out-dir]
+(defn write-project! [out-dir width depth]
   (let [src-dir (io/file out-dir "src")]
     (reset-src! src-dir)
-    (doseq [ns-name namespaces]
+    (doseq [[idx namespaces-at-depth] (map-indexed vector (namespace-levels width depth))
+            :let [depth-level (inc idx)]
+            ns-name namespaces-at-depth]
       (let [file-path (io/file src-dir (ns->file-path ns-name))
             parent (.getParentFile file-path)]
         (when parent (.mkdirs parent))
-        (spit file-path (generate-source ns-name default-depth default-width))))
-    (delete-generated-build-files! src-dir)))
+        (spit file-path (generate-source ns-name depth-level depth width))))))
 
 (defn -main [& _args]
   (let [ws-root (or (System/getenv "BUILD_WORKSPACE_DIRECTORY") ".")
@@ -84,15 +94,12 @@
     (println "  depth:" default-depth)
     (println "  output:" out-dir)
 
-    (let [start (System/nanoTime)
-          namespaces (generate-namespaces default-width default-depth)
-          gen-elapsed (/ (- (System/nanoTime) start) 1e6)]
-      (println "  namespaces:" (count namespaces))
-      (let [start (System/nanoTime)]
-        (write-project! namespaces out-dir)
-        (let [write-elapsed (/ (- (System/nanoTime) start) 1e6)
-              total-elapsed (+ gen-elapsed write-elapsed)]
-          (println)
-          (println "Generated" (count namespaces) "namespaces in" (format "%.0f" total-elapsed) "ms")
-          (println "Output directory:" out-dir)
-          (println))))))
+    (let [namespace-count (count-namespaces default-width default-depth)
+          start (System/nanoTime)]
+      (println "  namespaces:" namespace-count)
+      (write-project! out-dir default-width default-depth)
+      (let [elapsed-ms (/ (- (System/nanoTime) start) 1e6)]
+        (println)
+        (println "Generated" namespace-count "namespaces in" (format "%.0f" elapsed-ms) "ms")
+        (println "Output directory:" out-dir)
+        (println)))))
